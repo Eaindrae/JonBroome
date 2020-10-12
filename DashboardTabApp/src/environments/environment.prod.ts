2bc7207528afa00d11b0d92b97ae83c1f9580058
eaindraepyonenaing <eaindraepyone.n@alfaconnections.com>
2020-10-12 11:38:09 +0630

    Add project files.

---
+++ DashboardTabApp/src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: "https://JonBroomeedpn.azurewebsites.net/api/Request/",
+
+    authConfig: {
+        instance: "https://login.microsoftonline.com/",
+        tenantId: "1af8b4c7-8e05-466b-ba3b-a0ada1e909a9",
+        clientId: "3846abf3-e13a-4bbd-8d34-75310f6ff326",
+        redirectUri: "/app-silent-end",
+        cacheLocation: "localStorage",
+        navigateToLoginRequestUrl: false,
+        extraQueryParameters: "",
+        popUp: true,
+        popUpUri: "/app-silent-start",
+        popUpWidth: 600,
+        popUpHeight: 535
+    },
+
+    // do not populate the following:
+    upn: "",
+    tid: "",
+};
