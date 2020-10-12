d202b2dd6a2e247c5007158800b57524f8236972
Eaindrae <50760339+Eaindrae@users.noreply.github.com>
2020-10-12 14:12:39 +0630

    Update environment.ts
    2

--- DashboardTabApp/src/environments/environment.ts
+++ DashboardTabApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://jonbroomeedpn.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://betyouwannawearamasknow.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
