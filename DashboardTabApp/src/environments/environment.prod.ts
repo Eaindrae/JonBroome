3ac83713784ce53908ed7a35a471fb74664bc1d6
Eaindrae <50760339+Eaindrae@users.noreply.github.com>
2020-10-22 10:37:54 +0630

    Update environment.prod.ts
    4

--- DashboardTabApp/src/environments/environment.prod.ts
+++ DashboardTabApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://tellmewhereyouwanneatqna.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://moepwintdemoqboted.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
