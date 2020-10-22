7380e548ab540e99b786f2f5c52530274cd7c7d9
Eaindrae <50760339+Eaindrae@users.noreply.github.com>
2020-10-22 17:49:01 +0630

    Update environment.prod.ts
    7

--- QuestionsTabApp/src/environments/environment.prod.ts
+++ QuestionsTabApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://whenwheretowhatever.azurewebsites.net/api/Request/",
-    selfUrl: "https://whenwheretowhatever-questions.azurewebsites.net",
+    apiBaseUrl: "https://sleepybroomeborme.azurewebsites.net/api/Request/",
+    selfUrl: "https://sleepybroomeborme-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
